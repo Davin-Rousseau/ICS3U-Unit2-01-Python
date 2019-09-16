0124b669931d9dfc98de8928feac374c31318abc
Ubuntu <ubuntu@ip-172-31-24-253.ec2.internal>
2019-09-16 17:24:31 +0000

    added program

---
+++ circle_math.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Davin Rousseau
+# Created on September 2019
+# This proogram calculates perimeter and area of a Circle
+# with radius 15mm
+
+import math
+
+
+def main():
+    print("If a Circle has a radius of 15mm:")
+    print("")
+    print("Area is {}mm^2".format(math.pi*15**2))
+    print("Perimeter is {}mm".format(2*math.pi*15))
+
+
+if __name__ == "__main__":
+    main()
